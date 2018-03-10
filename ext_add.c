bb4922a2feb411bf2216e5cf9cd7ed2a75a475c2
Yiğit Özkavcı <yigitozkavci8@gmail.com>
2018-03-10 13:05:20 +0300

    Initial commit

---
+++ ext_add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int a, b;
+  printf("Enter a: ");
+  scanf("%d", &a);
+  printf("Enter b: ");
+  scanf("%d", &b);
+  printf("Result: %d\n", a + b);
+}
